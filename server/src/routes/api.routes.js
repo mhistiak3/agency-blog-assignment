2c145ddc225f7793b6d93e542bdd4a6d58059db9
Istiak <mhistiak2@gmail.com>
2024-12-06 17:24:20 +0600

    make all member route

--- server/src/routes/api.routes.js
+++ server/src/routes/api.routes.js
@@ -8,6 +8,12 @@ import {
   updateBlogController,
 } from "../controllers/blog.controller.js";
 import { imageUpload } from "../middleware/multer.middleware.js";
+import {
+  createMemberController,
+  deleteMemberController,
+  readMembersController,
+  updateMemberController,
+} from "../controllers/member.controller.js";
 const router = express.Router();
 
 // login
@@ -25,6 +31,12 @@ router
   .delete(deleteBlogController)
   .put(imageUpload, updateBlogController);
 
-  
+router
+  .route("/admin/members")
+  .get(readMembersController)
+  .post(imageUpload, createMemberController)
+  .delete(deleteMemberController)
+  .put(imageUpload, updateMemberController);
+
 
 export default router;
